fbd9abf523c85c3d4d1a5739c33be2a58cb479d1
Tina Bryson <tinabryson@Tinas-MacBook-Pro.local>
2017-12-02 13:14:41 -0500

    added flipping function

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,16 +2,34 @@ import React, {Component} from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends Component {
+
+    constructor(){
+        super();
+        this.state = {isFlipped: false}
+    }
+
+    clickHandler() {
+        this.setState( {isFlipped: !this.state.isFlipped})
+    }
+
+
     render() {
+
+        var memoryCardInnerClass = "MemoryCardInner";
+
+        if(this.state.isFlipped){
+            memoryCardInnerClass += " flipped"
+        }
+
         return (
-            <div className="MemoryCard">
-                <div className="MemoryCardInner">
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
                     <div className='MemoryCardBack'>
-                        <h1><img src='https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png'/></h1>
+                        <img src='https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png'/>
                     </div>
 
                     <div className='MemoryCardFront'>
-                        <h4>∆</h4>
+                        ∆
                     </div>
                 </div>
             </div>
