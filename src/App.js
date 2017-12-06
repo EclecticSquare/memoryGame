b2b637d35e45016f71b5568617110421a20d7c07
Tina Bryson <tinabryson@Tinas-MacBook-Pro.local>
2017-12-05 19:48:28 -0500

    updated state

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import MemoryCardBack from './MemoryCard.js'
 
+
+function generateDeck() {
+  var symbols = ["√", "π", "∂","œ","÷","†","ø","ß"];
+  var deck = [];
+
+  for(i=0;i<16;i++){
+    deck.push({isFlipped:false, symbol:symbols[i%8]})
+  }
+  shuffle(deck)
+  return deck
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+}
+
+
+
 class App extends Component {
   // Game Logic
+  constructor() {
+    super();
+      this.state = {
+        deck: generateDeck(),
+        pickedCards: []
 
+      }
+  }
 
   //Renders the physical gameboard
   render() {
